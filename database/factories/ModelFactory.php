412028ad211a12511e451e55e43aa0cc8f614ffb
Emeka Augustine <cafafanscoders@gamil.com>
2019-04-22 11:19:25 -0400

    Created factory and seeder for books

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,10 +10,11 @@
 | database. Just tell the factory how a default model should look.
 |
 */
-
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\Book::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'author_id' => $faker->numberBetween(1, 50),
+        'title' => $faker->sentence(2, true),
+        'description' => $faker->sentence(6, true),
+        'price' => $faker->numberBetween(50, 250),
     ];
 });
