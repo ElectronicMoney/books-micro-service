097f26789e704b43107f399f6660ea9ba6f62ed7
Emeka Augustine <cafafanscoders@gamil.com>
2019-04-22 10:48:50 -0400

    added some routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/books', 'AuthorController@index');
+$router->post('/books', 'AuthorController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
